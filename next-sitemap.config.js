5d7f38db58b9d09cfdeae2560fa989f1db5f99c9
Rumeel Hussain <rumeelhussain@Rumeels-MacBook-Pro.local>
2022-10-26 22:48:36 +0400

    package lock file removed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
     siteUrl: 'https://main.d1aos5rwqljdla.amplifyapp.com',
-    generateRobotsTxt: true, // (optional),
-    outDir : "./out",
+    generateRobotsTxt: true // (optional),
+    
     // ...other options
   };
   
