b6ca99da04503839ce5e37fa170315e95b356c38
Rumeel Hussain <rumeelhussain@Rumeels-MacBook-Pro.local>
2022-10-28 11:37:37 +0400

    fetcher issue resolved

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -2,7 +2,7 @@ import fetch from "isomorphic-unfetch";
 
 // eslint-disable-next-line
 export default async function Fetcher(...args) {
-  const res = await fetch(...args,
+  const res = await fetch(...args
     );
   return res.json();
 }
